5eeb413d62a27e54784d813d78cf0c3cf07bbec6
Ruindows <junhaumhayden@hotmail.com>
2024-08-05 22:11:43 -0300

    Add LavaCode folder

---
+++ LavaCode/src/main/java/com/junhaum/lavacode/LavaCode.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.junhaum.lavacode;
+
+/**
+ *
+ * @author Junhaum
+ */
+public class LavaCode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
