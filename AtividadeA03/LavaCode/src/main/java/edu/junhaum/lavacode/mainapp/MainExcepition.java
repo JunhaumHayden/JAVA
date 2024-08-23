f95c768f863cb98ca87c22cb2aeffe50881ce91d
pendrive <junhaumhayden@hotmail.com>
2024-08-23 00:51:26 -0300

    Add new exceptions tests in MainExcepition file

--- AtividadeA03/LavaCode/src/main/java/edu/junhaum/lavacode/mainapp/MainExcepition.java
+++ AtividadeA03/LavaCode/src/main/java/edu/junhaum/lavacode/mainapp/MainExcepition.java
@@ -3,17 +3,11 @@ package edu.junhaum.lavacode.mainapp;
 import java.util.Date;
 
 import edu.junhaum.lavacode.DataSource.DataSource;
-import edu.junhaum.lavacode.domain.Ecategoria;
 import edu.junhaum.lavacode.domain.Pontuacao;
 import edu.junhaum.lavacode.domain.clientes.Cliente;
 import edu.junhaum.lavacode.domain.clientes.PessoaFisica;
 import edu.junhaum.lavacode.domain.ordemServicos.EStatus;
 import edu.junhaum.lavacode.domain.ordemServicos.OrdemDeServico;
-import edu.junhaum.lavacode.domain.veiculos.Cor;
-import edu.junhaum.lavacode.domain.veiculos.EtipoCombustivel;
-import edu.junhaum.lavacode.domain.veiculos.Marca;
-import edu.junhaum.lavacode.domain.veiculos.Modelo;
-import edu.junhaum.lavacode.domain.veiculos.Veiculos;
 import edu.junhaum.lavacode.exceptions.ExceptionLavacao;
 
 public class MainExcepition {
@@ -27,9 +21,13 @@ public class MainExcepition {
         
             Cliente cliente = new PessoaFisica("João", "11999999999", "joao@email.com","123.456.789-10", new Date(2001 - 01 - 01));
             // cliente.addVeiculos(null);  // Vai lançar ExceptionLavacao
+            cliente.addVeiculos(DataSource.getVeiculo(3));
+
+            Cliente cliente02 = new PessoaFisica("Maria", "11999999999", "Maria@email.com","123.456.789-10", new Date(2001 - 01 - 01));
+            //cliente02.addVeiculos(DataSource.getVeiculo(3));// Vai lançar ExceptionLavacao
 
         
-            OrdemDeServico os = new OrdemDeServico(0,new Veiculos("ABC1111", "Veículo em boas condições", cliente, new Cor("Branco"),new Modelo("Civic", new Marca("Honda"), Ecategoria.MEDIO, 112, EtipoCombustivel.FLEX )));
+            OrdemDeServico os = new OrdemDeServico(0,DataSource.getVeiculo(3));
             os.setStatus(EStatus.FECHADA);
             // os.addItemOS(null);  // Vai lançar ExceptionLavacao
 
