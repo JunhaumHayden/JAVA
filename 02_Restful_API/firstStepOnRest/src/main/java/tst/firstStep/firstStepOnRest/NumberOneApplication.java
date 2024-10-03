8824866dc550df1d19700345f4fb8e50e4ed536b
JunhaumHayden <junhaumhayden@hotmail.com>
2024-10-03 00:08:30 -0300

    Add my first exemplo with Rest

--- 02_Restful_API/firstStepOnRest/src/main/java/tst/firstStep/firstStepOnRest/NumberOneApplication.java
+++ 02_Restful_API/firstStepOnRest/src/main/java/tst/firstStep/firstStepOnRest/NumberOneApplication.java
@@ -1,7 +1,5 @@
 package tst.firstStep.firstStepOnRest;
 
-import javax.swing.Spring;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +20,7 @@ public class NumberOneApplication
 	}
 		//   The @GetMapping(“/hello”) tells Spring to use our hello() method to answer requests that get sent to the http://localhost:8080/hello address.
 		  @GetMapping("/hello")
-		//   The hello() method we’ve added is designed to take a String parameter called name, and then combine this parameter with the word "Hello" in the code. This means that if you set your name to "Amy" in the request, the response would be “Hello Amy”.
+		//   The hello() method we’ve added is designed to take a String parameter called name, and then combine this parameter with the word "Hello" in the code. This means that if you set your name to "Amy" in the request, the response would be “Hello Amy” (http://localhost:8080/hello?name=Amy).
 		// the @RequestParam is telling Spring to expect a name value in the request, but if it’s not there, it will use the word "World" by default.
 		  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) 
 		  {
