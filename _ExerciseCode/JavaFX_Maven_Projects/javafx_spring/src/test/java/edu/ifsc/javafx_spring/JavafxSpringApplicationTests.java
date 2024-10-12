85aef718a4b03b0e31ffe7d202e47d21791974bc
JunhaumHayden <junhaumhayden@hotmail.com>
2024-10-12 17:13:59 -0300

    Add test java with spring (not completed)

---
+++ _ExerciseCode/JavaFX_Maven_Projects/javafx_spring/src/test/java/edu/ifsc/javafx_spring/JavafxSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifsc.javafx_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafxSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
