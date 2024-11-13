5d7552aa67fc7418a128ef28587d05e09cd2d3ec
JunhaumHayden <junhaumhayden@hotmail.com>
2024-11-13 17:02:11 -0300

    Add Produtos endpoints

---
+++ 02_Restful_API/rest_DTO_exemplo/src/main/java/edu/ifsc/rest_DTO_exemplo/controller/ProdutoController.java
@@ -0,0 +1,99 @@
+package edu.ifsc.rest_DTO_exemplo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.ifsc.rest_DTO_exemplo.domain.Categoria;
+import edu.ifsc.rest_DTO_exemplo.domain.Produto;
+import edu.ifsc.rest_DTO_exemplo.repository.CategoriaRepository;
+import edu.ifsc.rest_DTO_exemplo.repository.ProdutoRepository;
+
+/**
+ * Terceiro criar a classe que controlará o acesso a classe do dominio do problema
+ */
+@RequestMapping("/produtos") //para mapear uma URI padrao
+@RestController
+public class ProdutoController {
+    
+    @Autowired
+    private ProdutoRepository produtoRepository;
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    
+    @GetMapping
+    public List<Produto> listar() {
+        return produtoRepository.findAll();
+
+    }
+
+    /**
+     * Este metodo não contem uma resposta adequada a chamada http
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public Produto buscar(@PathVariable int id){
+        return produtoRepository.findById(id).get();
+    }
+    /**
+     * Este metodo contem uma resposta adequada para a requisiçao http
+     * @param id
+     * @return
+     */
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<Produto> buscarOtimizado(@PathVariable int id){
+        return produtoRepository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build()); //o metodo orElse é executado caso diferente de ok no metodo map(). Este é responsavel por alterar a mensagem de retorno de 500 para 404 na requisiçao.
+    }
+
+    /**
+     * Este metodo insere e retorna o objeto
+     * @param produto
+     * @return
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // Para alterar o retorno da chamada http de 200 para 201
+    public Produto inserir(@RequestBody Produto produto){
+        // return produtoRepository.save(produto); // executa um retorno lazy
+        produto = produtoRepository.save(produto);
+        Optional<Categoria> categoria = categoriaRepository.findById(produto.getCategoria().getId());
+        produto.setCategoria(categoria.get());
+        return produto;
+    }
+
+    @PutMapping("/{id}")
+    // public Produto atualizar ////Deixando assim sempre retorna 200 para a chamada http mesmo se atualizar um id que não existe.
+    public ResponseEntity<Produto> atualizar(@PathVariable Integer id,@RequestBody Produto produto){ //RequestBody indica que o parametro será retirado do Body da mensagem
+        //if(produtoRepository.existsById(id)) { //Deixando assim sempre retorna 200 para a chamada http mesmo se atualizar um id que não existe.
+        if(!produtoRepository.existsById(id)) { //Se não existir
+            return ResponseEntity.notFound().build(); //retorna not found
+        }else {
+            produto.setId(id);
+            return ResponseEntity.ok(produtoRepository.save(produto));
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Integer id){
+        if(!produtoRepository.existsById(id)){
+            return ResponseEntity.notFound().build(); //build para construir uma mensagem. Altera o codigo de retorno HPPT
+        } else{
+            produtoRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        
+    }
+
+}
---
+++ 02_Restful_API/rest_DTO_exemplo/src/main/java/edu/ifsc/rest_DTO_exemplo/domain/Produto.java
@@ -0,0 +1,59 @@
+package edu.ifsc.rest_DTO_exemplo.domain;
+/**
+ * Primeiro cria-se a classe que representará o objeto do dominio do problema
+ */
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+/**
+ * Essa anotação, quando aplicada à classe, instrui o Lombok a gerar automaticamente métodos "getter" para todos os campos não estáticos dessa classe.
+No caso da classe Produto, ela cria métodos como getId(), getNome(), getDescricao(), getPreco() e getCategoria().
+ */
+@Getter
+
+/**
+ * O @Setter import lombok.Getter é utilizado para que o Lombok gere automaticamente métodos "setter" para todos os campos não estáticos da classe.
+Para Produto, ele cria métodos como setId(Integer id), setNome(String nome), setDescricao(String descricao), setPreco(BigDecimal preco) e setCategoria(Categoria categoria).
+Esses métodos permitem modificar os valores dos campos da instância da classe Produto.
+ */
+@Setter
+
+/**
+ * Essa anotação configura o Lombok para gerar métodos equals() e hashCode() que serão baseados apenas no campo id.
+Com isso, dois objetos Produto serão considerados iguais se seus campos id forem iguais, independente dos valores dos outros campos.
+Essa abordagem é útil para evitar comparações excessivas de campos, especialmente em entidades onde o id é um identificador único no banco de dados.
+ */
+@EqualsAndHashCode(of={"id"})
+/**
+ *  @Entity importado de jakarta.persistence.Entity para mapear esta classe com o banco de dados
+ */
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    /**
+     * Anotaçao import jakarta.persistence.ManyToOne para definir a multiplicidade entre as classes (no caso de produto para categoria)
+     */
+    @ManyToOne
+    /**
+     * Anotaçao import jakarta.persistence.JoinColumn para definir a coluna que servirá de chave estrangeira  entre as classes (no caso de produto para categoria)
+     */
+    @JoinColumn(name="id_categoria")
+    private Categoria categoria;
+
+
+
+}
