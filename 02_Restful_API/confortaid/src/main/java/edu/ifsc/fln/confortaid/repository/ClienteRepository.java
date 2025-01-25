2493cc85fb2714a0a23d4572481c6dc772c5a214
JunhaumHayden <junhaumhayden@hotmail.com>
2025-01-24 23:37:20 -0300

    Update confortaid project

--- 02_Restful_API/confortaid/src/main/java/edu/ifsc/fln/confortaid/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.ifsc.fln.confortaid.repository;
-
-import edu.ifsc.fln.confortaid.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-}
-
--- 02_Restful_API/confortaid/src/main/java/edu/ifsc/fln/confortaid/repository/ProfissionalRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.ifsc.fln.confortaid.repository;
-
-import edu.ifsc.fln.confortaid.model.Profissional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfissionalRepository extends JpaRepository<Profissional, Integer> {
-}
-
